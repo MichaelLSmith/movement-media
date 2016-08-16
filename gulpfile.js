50efc0ac49c9ec5a08d733e87745a14ba9a21f18
MichaelLSmith <mlsmith45@gmail.com>
2016-08-16 12:05:45 -0400

    reorganized css files; processing to ./style.css

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
   animation = require('postcss-animation'),
 
   source = 'process/css/', //source css files
-  dest = 'builds'; //build file
+  dest = '.'; //build file
 
 //gulp.task('html', function() {
 //  gulp.src(dest + '*.html');
@@ -41,5 +41,3 @@ gulp.task('watch', function() {
 
 gulp.task('default', ['css', 'watch']);
 //'webserver', add after css
-
-
