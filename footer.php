4f7fd83e5f700500b58073d939e029bc774797b0
MichaelLSmith <mlsmith45@gmail.com>
2016-08-23 17:45:31 -0400

    have footer menu--no styles

--- footer.php
+++ footer.php
@@ -14,6 +14,14 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div id="footer-sidebar" class="secondary">
+			<div id="footer-sidebar1">
+				<?php
+				if(is_active_sidebar('footer-sidebar-1')){
+				dynamic_sidebar('footer-sidebar-1');
+				}
+				?>
+			</div>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'movementmedia' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'movementmedia' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
