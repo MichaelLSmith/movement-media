954ca4ab5b8c16733d817364ba3e6f77fd541fa8
MichaelLSmith <mlsmith45@gmail.com>
2016-08-24 19:13:20 -0400

    have footer image & address in

--- footer.php
+++ footer.php
@@ -30,7 +30,16 @@
 		 	Also want to be able to enter address.-->
 		</div>
 		<div class="footer-address-container">
-			<p><?php the_field('street_address', 'option'); ?></p>
+			<ul>
+				<li><?php the_field('street_address', 'option'); ?></li>
+				<li><?php the_field('city', 'option'); ?></li>
+				<li><?php the_field('state', 'option'); ?></li>
+				<li><?php the_field('zip_code', 'option'); ?></li>
+				<li><?php the_field('phone_number', 'option'); ?></li>
+			</ul>
+		</div>
+		<div class="footer-image-container">
+			<img src="<?php the_field('footer_image', 'option'); ?>" alt=""> 
 		</div>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'movementmedia' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'movementmedia' ), 'WordPress' ); ?></a>
