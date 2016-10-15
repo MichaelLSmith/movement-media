81e7f2cc8fc75cbce2a33bb9013ea954a5441677
MichaelLSmith <mlsmith45@gmail.com>
2016-10-15 11:21:56 -0400

    reorg front header

--- front-page.php
+++ front-page.php
@@ -9,7 +9,48 @@
  * @package MovementMedia
  */
 
-get_header(); ?>
+// get_header(); ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'movementmedia' ); ?></a>
+
+	<header id="home-masthead" class="home-header" role="banner">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<?php esc_html_e( 'Primary Menu', 'movementmedia' ); ?>
+			</button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
+		<section class="logo-tag-container">
+			<div class="site-logo">
+				<a href="<?php echo esc_url(home_url( '/' )); ?>" rel="home">
+		    	<div class="screen-reader-text">
+		        	<?php printf( esc_html__('Go to the home page of %1$s', 'movementmedia'), $site_title ); ?>
+		      	</div>
+	      		<?php the_custom_logo(); ?>
+			</div>
+
+			<div class="site-branding">
+			      <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+			      <p class="site-title"><?php bloginfo( 'description'); ?></p>
+			</div><!-- .site-branding -->
+		</section>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
