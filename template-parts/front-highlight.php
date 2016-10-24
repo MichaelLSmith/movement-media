30acd0802803844922e82c749b72071cf6273f35
MichaelLSmith <mlsmith45@gmail.com>
2016-10-24 08:11:52 -0400

    highlights working with placeholder image

--- template-parts/front-highlight.php
+++ template-parts/front-highlight.php
@@ -12,8 +12,9 @@
                 $post = $post_object;
                 setup_postdata( $post );
             ?>
+            <img src="http://localhost:8080/wp-content/uploads/2016/10/fairTrade.png" alt="" />
                <?php
-                the_field('video_url'); ?>
+                // the_field('video_url'); ?>
         <?php
          wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
         <?php endif;?>
@@ -24,7 +25,9 @@
         <?php
         if($post_object):
         elseif (has_post_thumbnail() ) : ?>
-            <?php the_post_thumbnail();?>
+        <img src="http://localhost:8080/wp-content/uploads/2016/10/fairTrade.png" alt="" />
+            <?php
+            //  the_post_thumbnail();?>
         <?php endif; ?>
             <div id="link">
                 <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
