99e3d52025b160639ca071b9860ef568c19b991a
MichaelLSmith <mlsmith45@gmail.com>
2016-11-16 15:39:23 -0500

    added testimonials.php

---
+++ page-templates/testimonials.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template Name: Testimonials
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package MovementMedia
+ */
+
+get_header(); ?>
+
+	<!-- <div id="primary" class="content-area"> -->
+		<main class="content-inner" role="main">
+			<article class="testimonials-outer-container full-content-area">
+				<section class="entry-title-container full-content-area">
+					<!-- might need to add a div here to help control width of title -->
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</section>
+
+                <section class="testimonial-intro bold-lead-paragraph full-content-area">
+					<div class="about-mission-inner bold-lead-paragraph-inner">
+						<?php the_field('testimonials_introduction'); ?>
+					</div>
+				</section>
+
+				<section class="testimonials-list full-content-area row-tb-pad">
+					<?php if( have_rows('testimonials_list') ): ?>
+						<?php while( have_rows('testimonials_list') ): the_row();
+
+						// vars
+						$image = get_sub_field('testimonial_image');
+
+						?>
+						<div class="testimonial">
+                            <div class="testimonial-flex">
+							    <?php if( $image ): ?>
+							    <div class="testimonial-image bio-image-container">
+							        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+							    </div>
+							    <?php endif; ?>
+							    <div class="testimonial-biline">
+							        <div class="testimonial-name">
+							            <h3><?php the_sub_field('testimonial_name');?></h3>
+							        </div>
+							        <div class="testimonial-title">
+							            <h3><?php the_sub_field('testimonial_title');?></h3>
+							        </div>
+                                    <div class="testimonial-url">
+                                        <h3>
+                                            <a href="<?php echo the_sub_field('testimonial_url');?>"><?php echo the_sub_field('testimonial_url');?></a>
+                                        </h3>
+                                    </div>
+							    </div>
+                                <div class="testimonial-text">
+                                    <?php the_sub_field('testimonial_text');?>
+                                </div>
+                            </div>
+                    <?php endwhile;//testimonials_list ?>
+					<?php endif;//testimonials_list ?>
+				</section>
+			</article>
+
+<!-- Thou surly doghearted bladder! Thou wimpled pox-marked whey-face! Thou qualling milk-livered measle! You are a tosser, wanker, git and mankey! A nutter, a lazy sod, a cream faced loon! Thou art as loathsome as a toad. Peace, ye fatguts! I do desire we may be better strangers.  Thou art a flesh-monger, fool and coward. You scullion, your rampillian, you fustilarian. I’ll tickle your catastrophe. Thou art as fat as butter. -->
+
+
+
+		</main><!-- #main -->
+	<!-- </div><! #primary -->
+</div><!-- #content -->
+<?php
+get_sidebar();
+get_footer();
